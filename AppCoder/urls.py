2f1437a3b6e8db32e551ba6e72ef1d36b7dc55e1
Axel Tremouilleres <axel.tremouilleres@gmail.com>
2023-10-03 23:42:09 -0300

    resubido

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from AppCoder import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('app/', include("AppCoder.urls")),
-    
+    path('productos/', views.lista_productos, name='lista_productos'),
+    path('crear_producto/', views.crear_producto, name='crear_producto'),
+    path('buscar_producto/', views.buscar_productos, name='buscar_productos'),
 ]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from AppCoder.models import Categoria, Producto, Pedido
+from .forms import CategoriaForm, ProductoForm, PedidoForm
 
-# Create your views here.
+def lista_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'AppCoder/lista_productos.html', {'productos': productos})
+
+def crear_producto(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_productos')
+    else:
+        form = ProductoForm()
+    return render(request, 'AppCoder/crear_producto.html', {'form': form})
+
+def buscar_productos(request):
+    if request.method == 'POST':
+        consulta = request.POST['consulta']
+        productos = Producto.objects.filter(nombre__icontains=consulta)
+    else:
+        productos = Producto.objects.all()
+    return render(request, 'AppCoder/buscar_productos.html', {'productos': productos})
+
+
+
